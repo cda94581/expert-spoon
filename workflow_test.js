d84e32c7ac46c9f72b8830b516078d73c36920f4
cda94581 <76138236+cda94581@users.noreply.github.com>
2022-11-26 15:45:57 -0600

    Sharing between jobs?

--- workflow_test.js
+++ workflow_test.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 console.log(fs.readdirSync('.', 'utf-8'));
-fs.writeFileSync(process.env.GITHUB_ENV, 'TEST=hi how are you');
+fs.writeFileSync(process.env.GITHUB_OUTPUT, 'TEST=hi how are you');
 console.log(fs.readdirSync('.', 'utf-8'));
